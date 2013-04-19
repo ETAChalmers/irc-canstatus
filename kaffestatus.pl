ccaad3ed819e20e1c04514f1879510fa389ac932
Anton Landberg <git@atnon.se>
2013-04-19 21:05:33 +0200

    Bot can now respond to private messages.

--- kaffestatus.pl
+++ kaffestatus.pl
@@ -6,14 +6,15 @@ use Irssi;
 our $VERSION = '1.00';
 our %IRSSI = (
     authors     => 'Anton Landberg',
-    contact     => 'bitter@eta.chalmers.se',
+    contact     => 'git@atnon.se',
     name        => 'ETAkaffestatus',
     description => 'Lyssnar efter fråga kring kaffe ställd till '.
 		   'Uppkopplad användare',
     license     => 'GNU GPLv3',
 );
 
-Irssi::signal_add_last 'message public', 'sig_message_public';
+Irssi::signal_add_first 'message public', 'sig_message_public';
+Irssi::signal_add_first 'message private', 'sig_message_private';
 
 sub sig_message_public {
      my ($server, $msg, $nick, $nick_addr, $target) = @_;
@@ -24,3 +25,10 @@ sub sig_message_public {
           $server->command("msg $target $nick: $KaffeStatus") if ($msg =~ m/$MyNick:.*?kaffe.*?/i);
      }
 }
+
+sub sig_message_private {
+     my ($server, $msg, $nick, $nick_addr) = @_;
+     my $KaffeStatus = `python /home/eta2/kaffestatus.py`;
+     $server->command("msg $nick $KaffeStatus") if ($msg =~ m/.*?kaffe.*?/i);
+}
+     
