b591ebf53e52f5d59b382c1ec170b7f2a35771bb
Anton Landberg <git@atnon.se>
2015-03-20 13:18:21 +0100

    Fixed indentation issues.
    Signed-off-by: Anton Landberg <git@atnon.se>

--- kaffestatus.pl
+++ kaffestatus.pl
@@ -5,30 +5,30 @@ use Irssi;
 
 our $VERSION = '1.00';
 our %IRSSI = (
-    authors     => 'Anton Landberg',
-    contact     => 'git@atnon.se',
-    name        => 'ETAkaffestatus',
-    description => 'Lyssnar efter fråga kring kaffe ställd till '.
-		   'uppkopplad användare',
-    license     => 'GNU GPLv3',
+    authors     =>  'Anton Landberg',
+    contact     =>  'git@atnon.se',
+    name        =>  'ETAkaffestatus',
+    description =>  'Lyssnar efter fråga kring kaffe ställd till '.
+                    'uppkopplad användare',
+    license     =>  'GNU GPLv3',
 );
 
 Irssi::signal_add_first 'message public', 'sig_message_public';
 Irssi::signal_add_first 'message private', 'sig_message_private';
 
 sub sig_message_public {
-     my ($server, $msg, $nick, $nick_addr, $target) = @_;
-     my $MyNick = $server->{nick};
-     my $KaffeStatus = "Vem vet?";
-     if ($target =~ m/#eta/i) { # only operate in these channels
-	  my $KaffeStatus = `python /home/eta2/kaffestatus.py`;
-          $server->command("msg $target $nick: $KaffeStatus") if ($msg =~ m/$MyNick:.*?kaffe.*?/i);
-     }
+    my ($server, $msg, $nick, $nick_addr, $target) = @_;
+    my $MyNick = $server->{nick};
+    my $KaffeStatus = "Vem vet?";
+    if ($target =~ m/#eta/i) { # only operate in these channels
+        my $KaffeStatus = `python /home/eta2/kaffestatus.py`;
+        $server->command("msg $target $nick: $KaffeStatus") if ($msg =~ m/$MyNick:.*?kaffe.*?/i);
+    }
 }
 
 sub sig_message_private {
-     my ($server, $msg, $nick, $nick_addr) = @_;
-     my $KaffeStatus = `python /home/eta2/kaffestatus.py`;
-     $server->command("msg $nick $KaffeStatus") if ($msg =~ m/.*?kaffe.*?/i);
+    my ($server, $msg, $nick, $nick_addr) = @_;
+    my $KaffeStatus = `python /home/eta2/kaffestatus.py`;
+    $server->command("msg $nick $KaffeStatus") if ($msg =~ m/.*?kaffe.*?/i);
 }
-     
+
--- kaffestatus.py
+++ kaffestatus.py
@@ -12,27 +12,28 @@ response.close()
 answer = ""
 
 # Format reference:
-#	%(antal)s - Number of cups
-#	%(tid)s - Time since last brewing.
+#   %(antal)s - Number of cups
+#   %(tid)s - Time since last brewing.
 
 BrewingAnswer = [
-		'Det bryggs kaffe för fullt!',
-		'Snart finns det svart guld i cisternen.', 
-		'Räddningen är nära, kaffet är på gång!', 
-		'Det är fritt fram att titta förbi, ty kaffet är snart klart.'
-		]
+    'Det bryggs kaffe för fullt!',
+    'Snart finns det svart guld i cisternen.', 
+    'Räddningen är nära, kaffet är på gång!', 
+    'Det är fritt fram att titta förbi, ty kaffet är snart klart.'
+]
+
 CoffieAnswer =  [
-		'Visst finns det kaffe! Senast bryggdes det %(antal)s koppar och det var %(tid)ssedan.',
-		'Är du sugen på kaffe så kanske det finns något kvar om du har tur, det bryggdes %(antal)s koppar för %(tid)ssedan.',
-		'Någon fantastisk människa har bryggt %(antal)s koppar för %(tid)ssedan!'
-		]
+    'Visst finns det kaffe! Senast bryggdes det %(antal)s koppar och det var %(tid)ssedan.',
+    'Är du sugen på kaffe så kanske det finns något kvar om du har tur, det bryggdes %(antal)s koppar för %(tid)ssedan.',
+    'Någon fantastisk människa har bryggt %(antal)s koppar för %(tid)ssedan!'
+]
 
 OldCoffieAnswer = [
-		  'Det var allt ett tag sedan det bryggdes, det finns nog inte kvar och skulle det mot förmodan göra det så gör du nog bättre i att brygga nytt då det är %(tid)s gammalt...',
-		  'Troligtvis inte.',
-		  'Om det mot förmodan skulle finnas något kaffe, så gör du nog bäst i att provsmaka först... En ålder på %(tid)sbådar inte gott.',
-		  'Se: http://www.youtube.com/watch?v=InsspuvAmBs'
-		]
+    'Det var allt ett tag sedan det bryggdes, det finns nog inte kvar och skulle det mot förmodan göra det så gör du nog bättre i att brygga nytt då det är %(tid)s gammalt...',
+    'Troligtvis inte.',
+    'Om det mot förmodan skulle finnas något kaffe, så gör du nog bäst i att provsmaka först... En ålder på %(tid)sbådar inte gott.',
+    'Se: http://www.youtube.com/watch?v=InsspuvAmBs'
+]
 
 last_update = datetime.datetime.strptime(data['coffie_nCups']['last_update'], '%Y-%m-%d %H:%M:%S')
 now = datetime.datetime.now()
@@ -40,22 +41,24 @@ diff =  now-last_update
 diff_hours = diff.seconds / 3600
 diff_mins = (diff.seconds / 60) % 60
 timestr = ""
-if (diff_hours > 0):
-	timestr = timestr + str(diff_hours) + "h "
-if (diff_mins > 0):
-	timestr = timestr + str(diff_mins) + "min "
 
+if (diff_hours > 0):
+    timestr = timestr + str(diff_hours) + "h "
+if (diff_mins > 0):
+    timestr = timestr + str(diff_mins) + "min "
 
 # Status 2 = brewing
 if data['coffie_status']['value'] == 2: # Coffie is brewing.
-	answer = random.choice(BrewingAnswer)
+    answer = random.choice(BrewingAnswer)
 elif data['coffie_nCups']['value'] < 12 and diff.seconds <= 5400: # Age is less than 1.5h.
-	answer = random.choice(CoffieAnswer) % {'antal' : data['coffie_nCups']['value'],
-						'tid': timestr
-						}
+    answer = random.choice(CoffieAnswer) % {
+        'antal' : data['coffie_nCups']['value'],
+        'tid': timestr
+        }
 else: # Coffie is most probably old.
-	answer = random.choice(OldCoffieAnswer) % {'antal' : data['coffie_nCups']['value'],
-						'tid': timestr
-						}
+    answer = random.choice(OldCoffieAnswer) % {
+        'antal' : data['coffie_nCups']['value'],
+        'tid': timestr
+        }
 
 print answer
